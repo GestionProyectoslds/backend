d1c7f086391875ab1e059e7d426175a31030bb71
Raul <raul12141@gmail.com>
2024-02-21 08:38:26 -0600

    Cronjob v1.0
    Cronjob extrayendo la informacion necesaria para las actualizaciones en timpo real(cada 10 min)

--- GDP_API/Cronjob.cs
+++ GDP_API/Cronjob.cs
@@ -5,6 +5,8 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using System.Data;
 using GDP_API.Data;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel;
 
 namespace GDP_API
 {
@@ -39,7 +41,50 @@ namespace GDP_API
                     {
                         table.Rows.Add(item.UserId, item.ActivitiesStatus, item.ActivitiesDeadline);
                     }
+                    //User total task status
+                    var q1 = from row in table.AsEnumerable()
+                             group row by new
+                             {
+                                 user = row.Field<string>("UserID"),
+                                 status = row.Field<string>("Status")
+                             }into grouped
+                             select new 
+                             {
+                                 users = grouped.Key.user,
+                                 ts = grouped.Key.status,
+                                 qs = grouped.Count()
+                             };
+                    //Show results in console (change this when the front-end is ready)
+                    foreach (var group in q1)
+                    {
+                        Console.WriteLine($"IdUsuario: {group.users}, Tarea status: {group.ts}, Numero de tareas: {group.qs}");
+                    }
+                    //User tasks comparing deadline with the current date to know if a task is on time or overdue
+                    DateTime currentDate= DateTime.Now.Date;
+                    var q2 = table.AsEnumerable().GroupBy(row => row.Field<string>("UserID"));
+                    foreach (var group in q2)
+                    {
+                        int onTime = 0;
+                        int overDue = 0;
+                        //Write in console user id delete after conected with front-end
+                        Console.WriteLine($"UserID: {group.Key}");
+                        foreach (var item in group)
+                        {
+                            DateTime dateInRow = (DateTime)item["Deadline"];
+                            if (dateInRow <= currentDate) 
+                            {
+                                onTime++;
+                            }
+                            else
+                            {
+                                overDue++;
+                            }
+                        }
+                        //Write in console on time and overdue task delete after conect with front-end
+                        Console.WriteLine("Tiene: ", onTime + overDue, " tareas totales y tiene: ", overDue," tareas atrasadas");
+                    }
                 }
+                
                 //10 min delay
                 await Task.Delay(600000);
             }
