e0080a61d619f116ab4ba8de6c553acbad6fff93
Raul <raul12141@gmail.com>
2024-03-19 11:19:52 -0600

    Expert users statics
    Se sacfan estadisticas para usuarios expertos y administradores.

--- GDP_API/Repositories/Interfaces/IStatisticsRepository.cs
+++ GDP_API/Repositories/Interfaces/IStatisticsRepository.cs
@@ -7,5 +7,15 @@ public interface IStatisticsRepository
     Task<int> GetExpertUsersCount();
     Task<int> GetActivitiesCount();
     Task<int> GetOverdueActivitiesCount();
+    Task<int> GetCompleteProjectsCount();
+    Task<int> GetInProgressProjectsCount();
     Task<IEnumerable<StatusCountDTO>> GetActivitiesCountByStatus();
+    Task<int> ExpertGetActiveProjectsCount(int userId);
+    Task<int> ExpertGetNormalUsersCount(int userId);
+    Task<int> ExpertGetExpertUsersCount(int userId);
+    Task<int> ExpertGetActivitiesCount(int userId);
+    Task<int> ExpertGetOverdueActivitiesCount(int userId);
+    Task<int> ExpertGetCompleteProjectsCount(int userId);
+    Task<int> ExpertGetInProgressProjectsCount(int userId);
+    Task<IEnumerable<StatusCountDTO>> ExpertGetActivitiesCountByStatus(int userId);
 }
--- GDP_API/Services/StatisticsService.cs
+++ GDP_API/Services/StatisticsService.cs
@@ -1,4 +1,9 @@
+using GDP_API;
+using GDP_API.Models;
 using GDP_API.Models.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.IdentityModel.Tokens.Jwt;
 public class StatisticsService : IHostedService, IStatisticsService
 {
     private readonly IServiceScopeFactory _serviceScopeFactory;
@@ -6,10 +11,11 @@ public class StatisticsService : IHostedService, IStatisticsService
     private readonly object _updateLock = new object();
     private Task? _backgroundTask;
     private CancellationTokenSource? _cts;
-
+    
     public StatisticsService(IServiceScopeFactory serviceScopeFactory)
     {
         _serviceScopeFactory = serviceScopeFactory;
+
         _statistics = new DashboardStatisticsDTO
         {
             ActiveProjectsCount = 0,
@@ -20,20 +26,59 @@ public class StatisticsService : IHostedService, IStatisticsService
             ActivitiesCountByStatus = new List<StatusCountDTO>()
         };
     }
-
+    
     public Task StartAsync(CancellationToken cancellationToken)
     {
         _cts = new CancellationTokenSource();
         _backgroundTask = ExecuteAsync(_cts.Token);
         return Task.CompletedTask;
     }
-
     public Task StopAsync(CancellationToken cancellationToken)
     {
         _cts!.Cancel();
         return Task.WhenAny(_backgroundTask!, Task.Delay(-1, cancellationToken));
     }
+    public async Task ExpertUserAsync(string jwt)
+    {
+        // Extract the user ID from the JWT
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var jwtToken = tokenHandler.ReadJwtToken(jwt);
+        //TODO - Research if claim type can be changed to use a .net constant instead of a string
+        //  ClaimTypes.NameIdentifier and  ClaimTypes.Name do not work
+        int userIdFromJwt = int.Parse(jwtToken.Claims.First(claim => claim.Type == "unique_name").Value);
+        int userTypeFromJwt = int.Parse(jwtToken.Claims.Last(claim => claim.Type == "unique_name").Value);
 
+        if ((userTypeFromJwt == 2) || (userTypeFromJwt == 3))
+        {
+            using (var scope = _serviceScopeFactory.CreateScope())
+            {
+                var repo = scope.ServiceProvider.GetRequiredService<IStatisticsRepository>();
+
+                var newStatistics = new DashboardStatisticsDTO
+                {
+                    ActiveProjectsCount = await repo.ExpertGetActiveProjectsCount(userIdFromJwt),
+                    NormalUsersCount = await repo.ExpertGetNormalUsersCount(userIdFromJwt),
+                    ExpertUsersCount = await repo.ExpertGetExpertUsersCount(userIdFromJwt),
+                    ActivitiesCount = await repo.ExpertGetActivitiesCount(userIdFromJwt),
+                    OverdueActivitiesCount = await repo.ExpertGetOverdueActivitiesCount(userIdFromJwt),
+                    CompleteProjectsCount = await repo.ExpertGetCompleteProjectsCount(userIdFromJwt),
+                    InProgressProjectsCount = await repo.ExpertGetInProgressProjectsCount(userIdFromJwt),
+                    ActivitiesCountByStatus = await repo.ExpertGetActivitiesCountByStatus(userIdFromJwt)
+                };
+
+                lock (_updateLock)
+                {
+                    _statistics = newStatistics;
+                }
+            }
+            await StopAsync(_cts.Token);
+            return;
+        }
+        else
+        {
+            return;
+        }
+    }
     protected async Task ExecuteAsync(CancellationToken stoppingToken)
     {
         while (!stoppingToken.IsCancellationRequested)
@@ -49,6 +94,8 @@ public class StatisticsService : IHostedService, IStatisticsService
                     ExpertUsersCount = await repo.GetExpertUsersCount(),
                     ActivitiesCount = await repo.GetActivitiesCount(),
                     OverdueActivitiesCount = await repo.GetOverdueActivitiesCount(),
+                    CompleteProjectsCount = await repo.GetCompleteProjectsCount(),
+                    InProgressProjectsCount = await repo.GetInProgressProjectsCount(),
                     ActivitiesCountByStatus = await repo.GetActivitiesCountByStatus()
                 };
 
